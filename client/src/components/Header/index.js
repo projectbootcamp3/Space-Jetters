99c6b87cce9b6618f289cb1de0bd5248872c0ba7
Lucca Araujo <lucca.ara7@gmail.com>
2022-07-19 21:37:11 -0700

    added header and footer components

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+   
+    return (
+      <header className="header">
+        <div className="header-subcontainer">
+         
+            <div className="logo-container"><h1>SpaceJetters</h1></div>
+          <nav className="nav" id="nav">
+            
+          </nav>
+        </div>
+      </header>
+    );
+};
+  
+  export default Header;
+  
