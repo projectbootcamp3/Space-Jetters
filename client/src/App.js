8794ffc3d34b85a8a30ef6c16b6ca31521d8c178
Lucca Araujo <lucca.ara7@gmail.com>
2022-07-19 22:01:30 -0700

    added the pages to the navbar using react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,50 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
+
+// Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from '@apollo/client/link/context';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Destinations from "./pages/Destinations";
+import Rockets from "./pages/Rockets";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
+
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -11,7 +53,14 @@ function App() {
         <Header />
         <div className="container">
           <Switch>
-            <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/destinations" component={Destinations} />
+              <Route exact path="/rockets" component={Rockets} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route component={NoMatch} />
           </Switch>
         </div>
         <Footer />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+      };
    
     return (
-      <header className="header">
+        <header className="header">
         <div className="header-subcontainer">
-         
+          <Link to="/">
             <div className="logo-container"><h1>SpaceJetters</h1></div>
+          </Link>
           <nav className="nav" id="nav">
-            
+            {Auth.loggedIn() ? (
+              <>
+                <Link to="/destinations">Destinations</Link>
+                <Link to="/rockets">Rockets</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/profile">Profile</Link>
+                <a href="/" onClick={logout}>
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+              </>
+            )}
           </nav>
         </div>
       </header>
