68f5e9d768a4950068fe183a5a501317aa4a5711
katochsenthal <katoch.senthal@gmail.com>
2022-07-27 19:52:28 -0400

    adding stripe

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
@@ -26,20 +26,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-};
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-};
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
