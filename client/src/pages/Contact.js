a48029e3f775a85dbf5b291b31b640a766c05846
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-07-28 11:31:43 -0500

    added style to contact-us page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,69 +1,73 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import {validateEmail} from '../utils/helpers';
+import contactBg from "../assets/contact-images/contactBg.jpg"
 
-import { validateEmail } from '../utils/helpers';
 
 function Contact() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+    function handleChange(event) {
+        if (event.target.name === 'email') {
+            const isValid = validateEmail(event.target.value);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!event.target.value.length) {
+                setErrorMessage(`${event.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if(!errorMessage) {
+            setFormState({...formState, [event.target.name]: event.target.value });
+        }
     }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState);
     }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
-
-  return (
-    <section>
-      <div id= "contact">
-      <h1 data-testid="h1tag">Contact us</h1>
-      </div>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
+    return (
+       <div className="contact-wrapper hero_image"
+       style={{
+         backgroundImage: `url(${contactBg})`,
+         backgroundSize: "cover",
+         height: "100vh",
+         backgroundPosition: "center",
+       }}>
+            <section id="contact">
+             <div className="subtitle-container">
+                <div className="line"></div>
+                <h2 className="sub-title">Contact Us</h2>
+                <div className="line"></div>
           </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
-    </section>
-  );
-}
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div className="flex">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                </div>
+                <div className="flex">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                </div>
+                <div className="flex">
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                </div >
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit"  data-testid="submit-btn" className=" btn-4 btn-3">Submit</button>
+            </form>
+        </section>
+       </div>
+        
+    );
+};
 
-
-export default Contact;
+export default Contact
