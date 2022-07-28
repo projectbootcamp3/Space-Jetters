1f084d1f4b94b99e0d847c6a689aad4caa41235c
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-07-28 11:56:51 -0500

    fixed errors

--- client/src/pages/Rockets.js
+++ client/src/pages/Rockets.js
@@ -3,25 +3,27 @@ import rocketImage1 from "../assets/rocket-images/Rocket-1.jpg";
 import rocketImage2 from "../assets/rocket-images/Rocket-2.jpg";
 
 const Rockets = () => {
-  const Rockets = [
-    {
-      name: "Sonic Rocket",
-      image: rocketImage1,
-    },
-    {
-      name: "Super-Sonic Rocket",
-      image: rocketImage2,
-    }
-  ];
+  // const rockets = [
+  //   {
+  //     name: "Sonic Rocket",
+  //     image: rocketImage1,
+  //   },
+  //   {
+  //     name: "Super-Sonic Rocket",
+  //     image: rocketImage2,
+  //   }
+  // ];
     
   return (
     <div class="rocketContainer">
     <div>
       <h2 className="sub-title"> Check available rockets</h2>
+      
     </div>
+  
     <section id="rocket-selection" class="rocket">
       <img 
-									src={rocketImage1} height={400} width={600} class="float-left"
+									src={rocketImage1} height={400} width={600} class="float-left" alt="Image of rocekt"
 								
 									
 								></img>
@@ -34,7 +36,7 @@ const Rockets = () => {
 
     <section id="superRocket-selection" class="rocket">
       <img 
-									src={rocketImage2} height={400} width={600} class="float-right"
+									src={rocketImage2} height={400} width={600} class="float-right" alt="Image of 2nd rocket"
 								
 									
 								></img>
