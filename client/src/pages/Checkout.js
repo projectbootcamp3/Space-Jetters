60ea9af71d787d3da6ef36053c407e65070a75ed
Lucca Araujo <lucca.ara7@gmail.com>
2022-07-28 18:09:38 -0700

    added nasa images page

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -3,11 +3,53 @@ import CheckoutForm from '../components/CheckoutForm/CheckoutForm'
 
 const Checkout = () => {
   return (
-    <div>
-      <div className="title-box">
-        <h2 className="sub-title">
-          <CheckoutForm />
-        </h2>
+    <div className="checkout-wrapper">
+      <div className="subtitle-container">
+        <div className="line"></div>
+            <h2 className="sub-title">Checkout Page</h2>
+        <div className="line"></div>
+      </div>
+
+      <div className="trip-infos">
+          <p>Destination: <span>Moon</span></p>
+          <p>Departure Date: <span>August 5, 2022</span></p>
+          <p>Crew Size: <span>5</span></p>
+          <p>Final Price: <span>$ 50.000</span></p>
+      </div>
+
+      <div className="card-details">
+        <p>Pay using Credit or Debit card</p>
+  
+        <div className="card-number">
+          <label> Card Number </label>
+          <input
+            type="text"
+            className="card-number-field"
+            placeholder="###-###-###"/>
+        </div>
+        <br />
+        <div className="date-number">
+          <label> Expiry Date </label>
+          <input type="text" className="date-number-field" 
+                 placeholder="DD-MM-YY" />
+        </div>
+  
+        <div className="cvv-number">
+          <label> CVV number </label>
+          <input type="text" className="cvv-number-field" 
+                 placeholder="xxx" />
+        </div>
+        <div className="nameholder-number">
+          <label> Card Holder name </label>
+          <input
+            type="text"
+            className="card-name-field"
+            placeholder="Enter your Name"/>
+        </div>
+        <button type="submit" 
+                className="submit-now-btn">
+          submit
+        </button>
       </div>
     </div>
   );
