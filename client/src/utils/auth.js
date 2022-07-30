4d28d8d86d3e55951379c82af960c2b052ee158c
pryority <matthewapryor@gmail.com>
2022-07-30 14:26:09 -0400

    commented out console.logs()

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -8,8 +8,8 @@ class AuthService {
   // Check if the user is still logged in
   loggedIn() {
     const token = this.getToken();
-    console.log('🪙 Encoded token: ', token)
-    console.log('User logged in. Is the token expired?', this.isTokenExpired(token));
+    // console.log('🪙 Encoded token: ', token)
+    // console.log('User logged in. Is the token expired?', this.isTokenExpired(token));
     return !!token && !this.isTokenExpired(token);
   }
 
@@ -17,7 +17,7 @@ class AuthService {
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
-      console.log('🔐 Decoded Token: ', decoded);
+      // console.log('🔐 Decoded Token: ', decoded);
       if (decoded.exp < Date.now() / 1000) {
         return true;
       } else {
@@ -30,14 +30,14 @@ class AuthService {
 
   // Get the data from the local storage
   getToken() {
-    console.log('GETTING TOKEN FROM LOCAL STORAGE', localStorage.getItem('id_token'));
+    // console.log('GETTING TOKEN FROM LOCAL STORAGE', localStorage.getItem('id_token'));
     return localStorage.getItem('id_token');
   }
 
   // Here is saving the user's token to local storage
   login(idToken) {
     localStorage.setItem('id_token', idToken);
-    console.log('💾 Setting id-token in local storage:', localStorage.setItem('id_token', idToken));
+    // console.log('💾 Setting id-token in local storage:', localStorage.setItem('id_token', idToken));
 
     window.location.assign('/');
     console.log('Returning to homepage, now with the updated, logged in UI!');
@@ -46,11 +46,11 @@ class AuthService {
   // Remove token to logout
   logout() {
     localStorage.removeItem('id_token');
-    console.log('USER LOGGED OUT -- Removing the idToken from local storage', localStorage.removeItem('id_token'));
+    // console.log('USER LOGGED OUT -- Removing the idToken from local storage', localStorage.removeItem('id_token'));
 
     // Reload the page
     window.location.assign('/');
-    console.log('👽 Abducted!......you logged out.')
+    // console.log('👽 Abducted!......you logged out.')
   }
 };
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -19,17 +19,17 @@ module.exports = {
     }
 
     if (!token) {
-      console.log('🌑 Oh! This user does not have a token! Here, passing the original request... ', req.body);
+      // console.log('🌑 Oh! This user does not have a token! Here, passing the original request... ', req.body);
       return req;
     }
 
     try {
       // returning the req.body here results in a graphQL query... need to figure out why it's the rockets query
       const { data } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { maxAge: expiration });
-      console.log(`Here is ${req.user.username}'s the verified data: `, data)
+      // console.log(`Here is ${req.user.username}'s the verified data: `, data)
       req.user = data;
     } catch {
-      console.log('🚫 Tried to verify the token, but it is invalid!');
+      // console.log('🚫 Tried to verify the token, but it is invalid!');
     }
 
     // console.log('🔐 Auth Request BODY: ', req.body);
@@ -38,7 +38,7 @@ module.exports = {
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     // In the log of payload, scroll up to see where the payload is delivered in the body of the request.
-    console.log('📄 PAYLOAD:', payload);
+    // console.log('📄 PAYLOAD:', payload);
     return jwt.sign({ data: payload }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: expiration });
   }
 };
